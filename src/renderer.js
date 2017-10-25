413eff81743efe80eb9d8b0f26a29ac2947bb38a
tett23 <tett23@gmail.com>
2017-10-25 15:44:37 +0900

    とりあえずファイルを開けるようにした

--- src/renderer.js
+++ src/renderer.js
@@ -1,16 +1,17 @@
+// @flow
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const {ipc} = require('electron');
+const { ipcRenderer } = require('electron');
 
-ipc.sendAsync('open-main-page', '', () => {
 
+// ipc.sendAsync('open-page');
+
+ipcRenderer.on('open-page', (event, arg) => {
+  console.log('open-page', arg);
 });
 
-ipc.sendAsync('open-page');
-
-ipc.on('open-page', () => {
-  console.log('open-page', arguments);
+ipcRenderer.send('open-main-page', '', () => {
+  console.log('hogehoge');
 });
-app
