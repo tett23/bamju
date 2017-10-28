f861c524f19d762ba2a6be3ff5f8f3a876d0aeb7
tett23 <tett23@gmail.com>
2017-10-29 08:15:16 +0900

    ファイルアクセスまわりを非同期に

--- app/main/project.js
+++ app/main/project.js
@@ -1,6 +1,7 @@
 // @flow
 
 import { ipcMain } from 'electron';
+import util from 'util';
 import * as Project from '../common/project';
 import { getBamjuConfig } from '../common/bamju_config';
 
@@ -8,45 +9,47 @@ const path = require('path');
 const fs = require('fs');
 const marked = require('marked');
 
-ipcMain.on('open-main-page', (event) => {
-  const buf = openFile('bamju-specifications', 'index.md');
+ipcMain.on('open-main-page', async (e) => {
+  const buf:Project.Buffer = await openFile('bamju-specifications', 'index.md');
 
-  event.sender.send('open-page', buf);
-  event.returnValue = buf;
+  e.sender.send('open-page', buf);
+  e.returnValue = buf;
 });
 
-ipcMain.on('open-page', (event, args) => {
-  const buf = openFile(args.projectName, args.itemName);
+ipcMain.on('open-page', async (e, args) => {
+  const buf:Project.Buffer = await openFile(args.projectName, args.itemName);
 
-  event.sender.send('open-page', buf);
-  event.returnValue = buf;
+  e.sender.send('open-page', buf);
+  e.returnValue = buf;
 });
 
-ipcMain.on('refresh-tree-view', (event, projectName: ?string) => {
+ipcMain.on('refresh-tree-view', async (e, projectName: ?string) => {
   let tree:Project.Projects = [];
   if (projectName != null) {
-    tree.push(loadProject(projectName));
+    tree.push(await loadProject(projectName));
   } else {
-    tree = loadProjects();
+    tree = await loadProjects();
   }
+  console.log('refresh-tree-view ret', tree);
 
-  event.sender.send('refresh-tree-view', tree);
-  event.returnValue = tree;
+  e.sender.send('refresh-tree-view', tree);
+  e.returnValue = tree;
 });
 
-const loadProjects = (): Project.Projects => {
+const loadProjects = async (): Promise<Project.Projects> => {
   const ret:Project.Projects = [];
 
   console.log('loadProjects', getBamjuConfig().projects);
-  Object.keys(getBamjuConfig().projects).forEach((projectName: string) => {
-    ret.push(loadProject(projectName));
-  });
+  const projectNames:Array<string> = Object.keys(getBamjuConfig().projects);
+  await Promise.all(projectNames.map(async (projectName: string) => {
+    ret.push(await loadProject(projectName));
+  }));
   console.log('loadProjects ret', ret);
 
   return ret;
 };
 
-const loadProject = (projectName: string): Project.Project => {
+const loadProject = async (projectName: string): Promise<Project.Project> => {
   const projectPath:string = getBamjuConfig().projects[projectName];
   if (projectPath === undefined) {
     throw new Error(`loadProject error${projectName}`);
@@ -57,22 +60,24 @@ const loadProject = (projectName: string): Project.Project => {
   const ret:Project.Project = {
     name: projectName,
     path: '/',
-    items: loadDirectory(projectPath, basePath)
+    items: await loadDirectory(projectPath, basePath)
   };
+  console.log('loadProject ret', ret);
 
   return ret;
 };
 
-const loadDirectory = (projectPath: string, basePath: string): Project.ProjectItems => {
-  const files = fs.readdirSync(projectPath);
+const loadDirectory = async (projectPath: string, basePath: string): Promise<Project.ProjectItems> => {
+  const readdir = util.promisify(fs.readdir);
+  const files:Array<string> = await readdir(projectPath);
   const ret:Project.ProjectItems = [];
-  files.forEach((filename: string) => {
+  await Promise.all(files.map(async (filename: string) => {
     const p:string = path.join(projectPath, filename);
     const itemType:Project.ItemType = Project.detectItemTypeByAbsPath(p);
 
     let items:Project.ProjectItems = [];
     if (itemType === Project.ItemTypeDirectory) {
-      items = loadDirectory(p, basePath);
+      items = await loadDirectory(p, basePath);
     }
 
     ret.push({
@@ -81,15 +86,15 @@ const loadDirectory = (projectPath: string, basePath: string): Project.ProjectIt
       itemType,
       items
     });
-  });
+  }));
 
   return ret;
 };
 
-const openFile = (projectName: string, itemName: string): Project.buffer => {
+const openFile = async (projectName: string, itemName: string): Promise<Project.Buffer> => {
   const projectPath:string = getBamjuConfig().projects[projectName];
   const abs:string = path.join(projectPath, itemName);
-  const stat:fs.Stat = fs.statSync(abs);
+  const stat:fs.Stats = await util.promisify(fs.stat)(abs);
   if (stat.isDirectory()) {
     return openDirectory(projectName, itemName);
   }
@@ -104,7 +109,7 @@ const openFile = (projectName: string, itemName: string): Project.buffer => {
 
   const itemType:Project.ItemType = Project.detectItemType(projectName, itemName);
 
-  const ret:Project.buffer = {
+  const ret:Project.Buffer = {
     name: itemName,
     path: path.join(projectName, itemName),
     itemType,
@@ -114,17 +119,17 @@ const openFile = (projectName: string, itemName: string): Project.buffer => {
   return ret;
 };
 
-const openDirectory = (projectName: string, itemName: string): Project.buffer => {
+const openDirectory = async (projectName: string, itemName: string): Promise<Project.Buffer> => {
   const projectPath:string = getBamjuConfig().projects[projectName];
   const abs:string = path.join(projectPath, itemName);
-  const files = fs.readdirSync(abs);
+  const files:Array<string> = await util.promisify(fs.readdir)(abs);
 
   let body:string = '';
   files.forEach((file: string) => {
     body += file;
   });
 
-  const ret:Project.buffer = {
+  const ret:Project.Buffer = {
     name: itemName,
     path: path.join(projectName, itemName),
     itemType: Project.ItemTypeDirectory,
