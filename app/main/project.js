b95a482ce3d8026db3f6f0f1175d32195b5b17e1
tett23 <tett23@gmail.com>
2017-11-21 02:57:38 +0900

    remove comments

--- app/main/project.js
+++ app/main/project.js
@@ -27,7 +27,6 @@ ipcMain.on('open-page', async (e, { projectName, itemName }) => {
 });
 
 ipcMain.on('refresh-tree-view', async (e) => {
-  // console.log('refresh-tree-view ret', manager.projects);
   const ret:Project.Projects = await Project.Manager.loadProjects();
 
   e.sender.send('refresh-tree-view', ret);
@@ -37,100 +36,3 @@ ipcMain.on('refresh-tree-view', async (e) => {
 ipcMain.on('open-by-editor', async (e, absolutePath: string) => {
   opn(absolutePath);
 });
-
-// const loadProjects = async (): Promise<Project.Projects> => {
-//   const ret:Project.Projects = [];
-//
-//   const projectNames:Array<string> = Object.keys(Config.projects);
-//   await Promise.all(projectNames.map(async (projectName: string) => {
-//     ret.push(await loadProject(projectName));
-//   }));
-//
-//   return ret;
-// };
-//
-// const loadProject = async (projectName: string): Promise<Project.Project> => {
-//   const projectPath:string = Config.projects[projectName];
-//   if (projectPath === undefined) {
-//     throw new Error(`loadProject error${projectName}`);
-//   }
-//
-//   const basePath:string = path.dirname(projectPath);
-//
-//   const ret:Project.Project = {
-//     name: projectName,
-//     path: '/',
-//     absolutePath: projectPath,
-//     itemType: 'project',
-//     items: await loadDirectory(projectPath, basePath)
-//   };
-//
-//   return ret;
-// };
-//
-// const loadDirectory = async (projectPath: string, basePath: string): Promise<Project.ProjectItems> => {
-//   const files:Array<string> = await util.promisify(fs.readdir)(projectPath);
-//   const ret:Project.ProjectItems = [];
-//   await Promise.all(files.map(async (filename: string) => {
-//     const p:string = path.join(projectPath, filename);
-//     const itemType:Project.ItemType = Project.detectItemTypeByAbsPath(p);
-//
-//     let items:Project.ProjectItems = [];
-//     if (itemType === Project.ItemTypeDirectory) {
-//       items = await loadDirectory(p, basePath);
-//     }
-//
-//     ret.push({
-//       name: filename,
-//       path: p.replace(basePath, ''),
-//       absolutePath: p,
-//       itemType,
-//       items
-//     });
-//   }));
-//
-//   return ret;
-// };
-//
-// const openFile = async (projectName: string, itemName: string): Promise<Project.Buffer> => {
-//   const projectPath:string = Config.projects[projectName];
-//   const abs:string = path.join(projectPath, itemName);
-//   const stat:fs.Stats = await util.promisify(fs.stat)(abs);
-//   if (stat.isDirectory()) {
-//     return openDirectory(projectName, itemName);
-//   }
-//
-//   const html:string = await Markdown.parse(projectName, abs);
-//   const itemType:Project.ItemType = Project.detectItemType(projectName, itemName);
-//
-//   const ret:Project.Buffer = {
-//     name: itemName,
-//     path: path.join(projectName, itemName),
-//     absolutePath: abs,
-//     itemType,
-//     body: html
-//   };
-//
-//   return ret;
-// };
-//
-// const openDirectory = async (projectName: string, itemName: string): Promise<Project.Buffer> => {
-//   const projectPath:string = Config.projects[projectName];
-//   const abs:string = path.join(projectPath, itemName);
-//   const files:Array<string> = await util.promisify(fs.readdir)(abs);
-//
-//   let body:string = '';
-//   files.forEach((file: string) => {
-//     body += file;
-//   });
-//
-//   const ret:Project.Buffer = {
-//     name: itemName,
-//     path: path.join(projectName, itemName),
-//     absolutePath: abs,
-//     itemType: Project.ItemTypeDirectory,
-//     body
-//   };
-//
-//   return ret;
-// };
