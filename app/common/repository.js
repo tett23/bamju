d98365fae925e12a098363ebb57f7afca32bfc1a
tett23 <tett23@gmail.com>
2018-02-05 23:30:31 +0900

    RepositoryManagerの空の状態

--- app/common/repository.js
+++ app/common/repository.js
@@ -1,7 +1,36 @@
 // @flow
 
-export class RepositoryManager {
+export type Buffer = {
+};
 
+type RepositoryConfig = Array<{
+  repositoryName: string,
+  absolutePath: string
+}>;
+
+let _repositories:Array<MetaData> = [];
+
+export class RepositoryManager {
+  static async init(buffers: Array<Buffer>, config: RepositoryConfig): Promise<Array<MetaData>> {
+    const ret = buffers.map((buffer) => {
+      return new MetaData(buffer);
+    });
+
+    _repositories = ret;
+
+    return ret;
+  }
+}
+
+
+export class FileItem {
+
+}
+
+export class MetaData {
+  constructor(buffer: Buffer) {
+
+  }
 }
 
 export default RepositoryManager;
