59bf1f42a2745ecb7be42caae71d1d0b214f455a
tett23 <tett23@gmail.com>
2018-02-09 15:35:08 +0900

    main.devを分割

--- app/main.dev.js
+++ app/main.dev.js
@@ -2,13 +2,6 @@
 // @flow
 
 import path from 'path';
-import { app } from 'electron';
-import { RepositoryManager } from './common/repository_manager';
-import { WindowManager } from './main/window';
-
-const { Config, defaultConfig } = require('./common/bamju_config');
-require('./main/window');
-require('./main/project');
 
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
@@ -21,53 +14,6 @@ if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true')
   require('module').globalPaths.push(p);
 }
 
-const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = [
-    'REACT_DEVELOPER_TOOLS',
-    'REDUX_DEVTOOLS'
-  ];
-
-  return Promise
-    .all(extensions.map(name => { return installer.default(installer[name], forceDownload); }))
-    .catch(console.log);
-};
-
-
-/**
- * Add event listeners...
- */
-
-app.on('window-all-closed', () => {
-  // Respect the OSX convention of having the application in memory even
-  // after all windows have been closed
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-
-app.on('ready', async () => {
-  if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
-    await installExtensions();
-  }
-  console.log('event app ready');
-
-  await Config.init();
-
-  const repositoryManager = new RepositoryManager(Config.bufferItems, Config.repositories);
-  repositoryManager.loadRepositories();
-
-  WindowManager.loadWindows(Config.windows);
-});
-
-app.on('before-quit', () => {
-  Config.quit();
-});
-
-app.on('activate', async () => {
-  if (WindowManager.getWindows().length === 0) {
-    WindowManager.createAppWindow(defaultConfig.windows[0]);
-  }
-});
+require('./main/app');
+require('./main/window');
+require('./main/project');
---
+++ app/main/app.js
@@ -0,0 +1,56 @@
+// @flow
+
+import { app } from 'electron';
+import devtoolsInstaller from 'electron-devtools-installer';
+import { RepositoryManager } from '../common/repository_manager';
+import { WindowManager } from '../main/window';
+import {
+  Config,
+  defaultConfig,
+} from '../common/bamju_config';
+
+
+app.on('window-all-closed', () => {
+  // Respect the OSX convention of having the application in memory even
+  // after all windows have been closed
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+
+app.on('ready', async () => {
+  if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
+    await installExtensions();
+  }
+  console.log('event app ready');
+
+  await Config.init();
+
+  const repositoryManager = new RepositoryManager(Config.bufferItems, Config.repositories);
+  repositoryManager.loadRepositories();
+
+  WindowManager.loadWindows(Config.windows);
+});
+
+app.on('before-quit', () => {
+  Config.quit();
+});
+
+app.on('activate', async () => {
+  if (WindowManager.getWindows().length === 0) {
+    WindowManager.createAppWindow(defaultConfig.windows[0]);
+  }
+});
+
+async function installExtensions() {
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = [
+    'REACT_DEVELOPER_TOOLS',
+    'REDUX_DEVTOOLS'
+  ];
+
+  Promise.all(extensions.map((name) => {
+    return devtoolsInstaller.default(devtoolsInstaller[name], forceDownload);
+  })).catch(console.log);
+}
