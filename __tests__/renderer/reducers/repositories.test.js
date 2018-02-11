b6449a7ca230068269684d46604687920d07a631
tett23 <tett23@gmail.com>
2018-02-11 17:19:57 +0900

    不要なimportの削除

--- __tests__/renderer/reducers/repositories.test.js
+++ __tests__/renderer/reducers/repositories.test.js
@@ -13,7 +13,6 @@ import {
   removeBuffers
 } from '../../../app/renderer/actions/repositories';
 import {
-  ItemTypeUndefined,
   createMetaDataID,
 } from '../../../app/common/metadata';
 import {
