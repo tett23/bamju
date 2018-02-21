c57fa2d08347efba2a83ef812d6cd3f490ae5c39
tett23 <tett23@gmail.com>
2018-02-21 18:35:06 +0900

    main/repositoryの削除

--- app/main.dev.js
+++ app/main.dev.js
@@ -1,11 +1,13 @@
 /* eslint global-require: 0, flowtype/no-weak-types: 0 */
 // @flow
 
+import fs from 'fs';
 import path from 'path';
 import { ipcMain } from 'electron';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { forwardToRenderer, replayActionMain } from 'electron-redux';
+import opn from 'opn';
 import {
   appReducer,
   initialState,
@@ -15,26 +17,13 @@ import {
 } from './main/event_dispatcher';
 
 import {
-  openBuffer,
-  openBySystemEditor,
-  createFile,
-} from './main/repository';
-
-import {
   getInstance,
 } from './common/repository_manager';
-import {
-  type MetaDataID,
-} from './common/metadata';
-import {
-  type Buffer,
-} from './common/buffer';
 // import {
 //   getInstance as getConfigInstance
 // } from './common/bamju_config';
 import {
-  type Message,
-  isSimilarMessage,
+  MessageTypeError,
 } from './common/util';
 
 import {
@@ -91,11 +80,21 @@ ipcMain.on('get-state', (e) => {
 
 ipcMain.on('open-by-system-editor', async (e, absolutePath: string) => {
   console.log('open-by-system-editor', absolutePath);
-  const result = openBySystemEditor(absolutePath);
-  if (isSimilarMessage(result)) {
-    e.sender.send('message', result);
-    e.returnValue = result;
+
+  try {
+    fs.statSync(absolutePath);
+  } catch (err) {
+    const message = {
+      type: MessageTypeError,
+      message: `open-by-system-editor stat error: ${err.message}. absolutePath=${absolutePath}`
+    };
+    e.sender.send('message', message);
+    e.returnValue = message;
     return;
+  }
+
+  if (process.env.NODE_ENV !== 'test') {
+    opn(absolutePath);
   }
 
   e.returnValue = true;
@@ -110,92 +109,53 @@ ipcMain.on('buffers', async (e) => {
 });
 
 // TODO: windowの更新
-ipcMain.on('create-file', async (e, arg: {repositoryName: string, path: string}) => {
-  console.log('create-file', arg);
-  const result:Buffer | Message = await createFile(arg);
-  if (result == null || isSimilarMessage(result)) {
-    e.sender.send('message', result);
-    e.returnValue = result;
-    return;
-  }
+// ipcMain.on('create-file', async (e, arg: {repositoryName: string, path: string}) => {
+//   console.log('create-file', arg);
+//   const result:Buffer | Message = await createFile(arg);
+//   if (result == null || isSimilarMessage(result)) {
+//     e.sender.send('message', result);
+//     e.returnValue = result;
+//     return;
+//   }
+//
+//   if (result.id == null) {
+//     return;
+//   }
+//
+//   const buffer:Buffer = (result: any);
+//
+//   const openPageResult = await openBuffer({ repositoryName: buffer.repositoryName, itemName: buffer.path });
+//   if (!isSimilarMessage(openPageResult)) {
+//     e.sender.send('open-buffer', openPageResult);
+//   }
+//
+//   const repo = getInstance().find(buffer.repositoryName);
+//   if (repo == null) {
+//     e.returnValue = null;
+//     return;
+//   }
+//
+//   let tmp = buffer;
+//   const parents = [];
+//   while (tmp.parentID != null) {
+//     const parent = repo.getItemByID(tmp.parentID);
+//     if (parent == null) {
+//       break;
+//     }
+//
+//     parents.push(parent);
+//     tmp = parent;
+//   }
+//
+//   const ret = {
+//     additons: [result],
+//     changes: parents,
+//   };
+//
+//   e.sender.send('file-created', ret);
+//   e.returnValue = ret;
+// });
 
-  if (result.id == null) {
-    return;
-  }
-
-  const buffer:Buffer = (result: any);
-
-  const openPageResult = await openBuffer({ repositoryName: buffer.repositoryName, itemName: buffer.path });
-  if (!isSimilarMessage(openPageResult)) {
-    e.sender.send('open-buffer', openPageResult);
-  }
-
-  const repo = getInstance().find(buffer.repositoryName);
-  if (repo == null) {
-    e.returnValue = null;
-    return;
-  }
-
-  let tmp = buffer;
-  const parents = [];
-  while (tmp.parentID != null) {
-    const parent = repo.getItemByID(tmp.parentID);
-    if (parent == null) {
-      break;
-    }
-
-    parents.push(parent);
-    tmp = parent;
-  }
-
-  const ret = {
-    additons: [result],
-    changes: parents,
-  };
-
-  e.sender.send('file-created', ret);
-  e.returnValue = ret;
-});
-
-ipcMain.on('close-item', async (e, metaDataID: MetaDataID) => {
-  console.log('close-item', metaDataID);
-  const result = await closeItem(metaDataID);
-  if (isSimilarMessage(result)) {
-    e.sender.send('message', result);
-    e.returnValue = result;
-    return;
-  }
-
-  const ret = {
-    changes: [result]
-  };
-
-  e.sender.send('update-buffers', ret);
-  e.returnValue = ret;
-});
-
-// TODO: loadをchainさせて非同期でupdate-buffersを発行？
-ipcMain.on('open-item', async (e, metaDataID: MetaDataID) => {
-  console.log('open-item', metaDataID);
-
-  const metaData = getInstance().getItemByID(metaDataID);
-  if (metaData == null) {
-    return;
-  }
-
-  const result = await openItem(metaDataID);
-  if (isSimilarMessage(result)) {
-    e.sender.send('message', result);
-    e.returnValue = result;
-    return;
-  }
-
-  const ret = getInstance().toBuffers();
-  console.log('ret', ret);
-
-  e.sender.send('reload-buffers', ret);
-  e.returnValue = ret;
-});
 
 require('./main/app');
 require('./main/window');
