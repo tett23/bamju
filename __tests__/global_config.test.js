ae4df3d0b21a95faf3955857794504661cb6ae5f
tett23 <tett23@gmail.com>
2018-03-04 00:05:27 +0900

    mock-fsを使う

--- __tests__/global_config.test.js
+++ __tests__/global_config.test.js
@@ -1,37 +1,18 @@
 /* eslint no-undef: 0, no-empty: 0 */
 
-import fs from 'fs';
+import mock from 'mock-fs';
 
 jest.setTimeout(500);
 beforeEach(() => {
-  try {
-    fs.mkdirSync('/tmp/bamju');
-  } catch (_) {
-  }
+  mock({
+    '/tmp/bamju': {}
+  });
 });
 
 afterEach(() => {
-  try {
-    rmForce('/tmp/bamju');
-  } catch (e) {
-    console.log(e);
-  }
+  mock.restore();
 });
 
 it('dummy', () => {
   expect(true).toBe(true);
 });
-
-function rmForce(path) {
-  if (fs.existsSync(path)) {
-    fs.readdirSync(path).forEach((file) => {
-      const curPath = `${path}/${file}`;
-      if (fs.lstatSync(curPath).isDirectory()) {
-        rmForce(curPath);
-      } else {
-        fs.unlinkSync(curPath);
-      }
-    });
-    fs.rmdirSync(path);
-  }
-}
