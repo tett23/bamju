f88f14844552ff55549369cdf1b4a6600f368537
tett23 <tett23@gmail.com>
2018-03-04 10:53:33 +0900

    不要な行を削る

--- app/renderer/containers/EditorApp.js
+++ app/renderer/containers/EditorApp.js
@@ -1,7 +1,6 @@
 // @flow
 
 import React from 'react';
-import { connect } from 'react-redux';
 import { Editor } from '../components/Editor';
 import { Messages } from '../components/Messages';
 
@@ -14,24 +13,5 @@ const app = () => {
   );
 };
 
-// // FIXME: いらない気がする
-// const defaultState = {
-//   treeView: initialTreeViewState(),
-//   browser: initialBrowserState()
-// };
-//
-const mapStateToProps = (state) => {
-  console.log('EditorApp mapStateToProps', state);
-
-  return state;
-};
-
-const mapDispatchToProps = (dispatch) => {
-  console.log('EditorApp mapDispatchToProps', dispatch);
-
-  return {};
-};
-
-const App = connect(mapStateToProps, mapDispatchToProps)(app);
-
-export default App;
+export const App = app;
+export default app;
