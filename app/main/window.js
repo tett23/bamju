0e81226c4023eb8e53477278f44433e518679254
tett23 <tett23@gmail.com>
2018-03-04 11:45:34 +0900

    保存時にTabの内容を更新

--- app/main/window.js
+++ app/main/window.js
@@ -12,35 +12,26 @@ import {
 import {
   type MetaDataID,
 } from '../common/metadata';
-import {
-  MessageTypeSucceeded,
-  MessageTypeFailed,
-} from '../common/message';
-import {
-  getInstance as getWindowManagerInstance
-} from '../common/window_manager';
+import * as Message from '../common/message';
+import * as dispatcher from './event_dispatcher';
+import { addMessage } from '../actions/messages';
+import { bufferContentUpdated } from '../actions/buffers';
 
 ipcMain.on('save-buffer', async (e, [metaDataID, content]: [MetaDataID, string]) => {
   console.log('save-buffer', metaDataID, content);
 
   const metaData = getRepositoryManagerInstance().getItemByID(metaDataID);
   if (metaData == null) {
-    const mes = {
-      type: MessageTypeFailed,
-      message: `save-buffer error: metaData not found metaDataID=${metaDataID}`,
-    };
+    const mes = Message.fail(`save-buffer error: metaData not found metaDataID=${metaDataID}`);
+    dispatcher.dispatch(addMessage(mes));
     e.sender.send('message', mes);
     e.returnValue = null;
     return;
   }
 
   const message = await metaData.updateContent(content);
-  if (message.type !== MessageTypeSucceeded) {
-    const mes = {
-      type: MessageTypeFailed,
-      message: `save-buffer updateContent error: ${message.message}`,
-    };
-    e.sender.send('message', mes);
+  if (Message.isSimilarError(message)) {
+    dispatcher.dispatch(addMessage(Message.wrap(message)));
     e.returnValue = null;
   }
 
@@ -48,15 +39,11 @@ ipcMain.on('save-buffer', async (e, [metaDataID, content]: [MetaDataID, string])
   e.returnValue = message;
 
   const [parseResult, parseMessage] = await metaData.parse();
-  if (parseResult == null || parseMessage.type !== MessageTypeSucceeded) {
-    const mes = {
-      type: MessageTypeFailed,
-      message: `save-buffer metaData.parse error: ${parseMessage.message}`,
-    };
-    e.sender.send('message', mes);
+  if (parseResult == null || Message.isSimilarError(parseMessage)) {
+    dispatcher.dispatch(addMessage(Message.wrap(parseMessage)));
     e.returnValue = null;
     return;
   }
 
-  getWindowManagerInstance().sendSavedEventAll(metaData.id, parseResult.content);
+  dispatcher.dispatch(bufferContentUpdated(metaData.id, parseResult.content));
 });
