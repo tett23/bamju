ee235b4a498eb5aaee31aa3d76710692e7ea4e0b
tett23 <tett23@gmail.com>
2018-03-11 04:20:37 +0900

    css

--- app/renderer/components/SearchProgress.js
+++ app/renderer/components/SearchProgress.js
@@ -13,20 +13,16 @@ type Props = {
 };
 
 function SearchProgress(props: Props) {
-  let circle = null;
-  if (!props.completed) {
-    circle = <div>loading</div>;
-  }
-
   return (
     <div className={styles.searchProgress}>
-      <ProgressBar {...props.progress} />
+      <div className={styles.progressBar}>
+        <ProgressBar {...props.progress} />
+      </div>
       <div className={styles.count}>
         <span>{props.progress.current}</span>
         <span>/</span>
         <span>{props.progress.total}</span>
       </div>
-      {circle}
     </div>
   );
 }
